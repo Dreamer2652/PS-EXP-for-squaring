62ae4da6b8ce267bbfd52a7c5c08aabe555b6641
Clite <42111572+Clite@users.noreply.github.com>
2023-09-17 00:29:31 +0900

    Update Sample_Fibonacci_by_squaring_matrix.c

--- Sample_Fibonacci_by_squaring_matrix.c
+++ Sample_Fibonacci_by_squaring_matrix.c
@@ -1,12 +1,11 @@
 // #11444 : 피보나치 수 6
 #include <stdio.h>
 
-long long n, result[2][2] = {1,0,0,1}, coeff[2][2] = {1,1,1,0};
+long long n, result[2][2] = {1,0,0,1}, coeff[2][2] = {1,1,1,0}, mod = 1e9+7, i, j, k;
 
 void MatMul(long long matrix[][2])
 {
   long long temp[5][5]={0,};
-  int mod = 1e9+7, i, j, k;
   
   // O((행렬 크기)³)
   for(i = 0; i < 2; i++)
@@ -24,10 +23,9 @@ int main()
   for(scanf("%lld",&n); n; n>>=1)
   {
     if(n & 1)
-      MatMul(result);  // result <= result × A
-    MatMul(coeff);    // A <= A × A
+      MatMul(result);  // result <= result × coeff 
+    MatMul(coeff);    // coeff <= coeff × coeff
   }
-  
   printf("%lld", result[1][0]);
   return 0;
 }
