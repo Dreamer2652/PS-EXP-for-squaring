7372a3e64606251276e9b9d37188e0fbc509d570
Clite <42111572+Clite@users.noreply.github.com>
2023-09-17 00:26:14 +0900

    Update Sample_EXP_by_squaring_matrix.c

--- Sample_EXP_by_squaring_matrix.c
+++ Sample_EXP_by_squaring_matrix.c
@@ -1,41 +1,12 @@
 // #10830 : 행렬 제곱
 #include <stdio.h>
 
-int N, result[5][5], A[5][5], i, j, k;
-
-void MatMul(int matrix[][5]);
-int main()
-{
-  long long B;
-  scanf("%d%lld", &N, &B);
-
-  for(i = 0; i < N; i++)
-    for(j = 0; j < N; j++)
-    {
-      scanf("%d", A[i]+j);      // scanf("%d", &A[i][j]);
-      result[i][j] = (i==j);    // 정수에서의 초기값 : 1 → 행렬에서의 초기값 : 단위 행렬
-    }
-
-  for(; B; B >>= 1)
-  {
-    if(B & 1)
-      MatMul(result);  // result <= result × A
-    MatMul(A);    // A <= A × A
-  }
-
-  for(i = 0; i < N; i++)
-  {
-    for(j = 0; j < N; j++)
-      printf("%d ", result[i][j]);
-    puts("");
-  }
-
-  return 0;
-}
+int N, result[5][5], A[5][5];
 
 void MatMul(int matrix[][5])
 {
-  int temp[5][5]={0,}, mod = 1e3;
+  int temp[5][5]={0,};
+  int mod = 1e3, i, j, k;
 
   // O((행렬 크기)³)
   for(i = 0; i < N; i++)
@@ -46,4 +17,38 @@ void MatMul(int matrix[][5])
   for(i = 0; i < N; i++)
     for(j = 0; j < N; j++)
       matrix[i][j] = temp[i][j];  // temp 행렬을 원래 행렬에 복사
+}
+
+int main()
+{
+  long long B;
+  int i, j;
+  
+  scanf("%d%lld", &N, &B);
+
+  // Initialize
+  for(i = 0; i < N; i++)
+    for(j = 0; j < N; j++)
+    {
+      scanf("%d", A[i]+j);      // scanf("%d", &A[i][j]);
+      result[i][j] = (i==j);    // 정수에서의 초기값 : 1 → 행렬에서의 초기값 : 단위 행렬
+    }
+
+  // Multiply
+  for(; B; B >>= 1)
+  {
+    if(B & 1)
+      MatMul(result);  // result <= result × A
+    MatMul(A);    // A <= A × A
+  }
+
+  // print
+  for(i = 0; i < N; i++)
+  {
+    for(j = 0; j < N; j++)
+      printf("%d ", result[i][j]);
+    puts("");
+  }
+
+  return 0;
 }
